878957f172e631278ed276644c2daa82ae9e7a90
Jan Algermissen <algermissen@acm.org>
2013-07-23 15:21:09 +0200

    -

--- test/test_crypto.c
+++ test/test_crypto.c
@@ -15,7 +15,7 @@ int test_that_keygen_generates_same_key() {
 	ciron_generate_key(&ctx,(unsigned char *)"password",  8, saltbuf, 64,
 			CIRON_DEFAULT_ENCRYPTION_OPTIONS->algorithm, 1, buf1);
 
-	ciron_generate_key(&ctx,(unsigned char *)"passwors",  8, saltbuf, 64,
+	ciron_generate_key(&ctx,(unsigned char *)"password",  8, saltbuf, 64,
 			CIRON_DEFAULT_ENCRYPTION_OPTIONS->algorithm, 1, buf2);
 
 	EXPECT_BYTE_EQUAL(buf1, buf2, NBYTES(CIRON_DEFAULT_ENCRYPTION_OPTIONS->algorithm->key_bits));
